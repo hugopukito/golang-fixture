889c7bf26e1cc09808486451a070c8706398c69f
hugopukito <gogo26hugop@gmail.com>
2023-08-09 18:58:59 +0200

    test

--- main.go
+++ main.go
@@ -2,10 +2,6 @@ package fixture
 
 import (
 	"fmt"
-
-	"github.com/hugopukito/golang-fixture/color"
-	"github.com/hugopukito/golang-fixture/database"
-	"github.com/hugopukito/golang-fixture/funcs"
 )
 
 // func main() {
@@ -14,20 +10,21 @@ import (
 
 func RunFixtures(databaseName, structsPackageName, fixtureDirName string) {
 
-	fmt.Println(color.Pink + "Testing connection to your sql..." + color.Reset)
-	database.InitDB(databaseName)
+	// fmt.Println(color.Pink + "Testing connection to your sql..." + color.Reset)
+	// database.InitDB(databaseName)
 
-	fmt.Println(color.Blue + "\nParsing your local structs..." + color.Reset)
-	funcs.InitLocalStructs(structsPackageName)
+	// fmt.Println(color.Blue + "\nParsing your local structs..." + color.Reset)
+	// funcs.InitLocalStructs(structsPackageName)
 
-	yamlFixtures, err := funcs.GetYamlStructs(fixtureDirName)
-	if err != nil {
-		fmt.Println(color.Red + "GetYamlStructs err: " + err.Error() + color.Reset)
-		return
-	}
+	// yamlFixtures, err := funcs.GetYamlStructs(fixtureDirName)
+	// if err != nil {
+	// 	fmt.Println(color.Red + "GetYamlStructs err: " + err.Error() + color.Reset)
+	// 	return
+	// }
 
-	fmt.Println(color.Purple + "Parsing your fixtures... \n" + color.Reset)
-	for _, yamlFixture := range yamlFixtures {
-		funcs.ParseFixture(yamlFixture, databaseName)
-	}
+	// fmt.Println(color.Purple + "Parsing your fixtures... \n" + color.Reset)
+	// for _, yamlFixture := range yamlFixtures {
+	// 	funcs.ParseFixture(yamlFixture, databaseName)
+	// }
+	fmt.Println("hey")
 }
